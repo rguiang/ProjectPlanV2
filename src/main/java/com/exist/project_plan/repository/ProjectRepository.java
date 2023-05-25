7a3dd76a005963ccc8ca764162e38fd7946cbd74
rguiang <118701960+rguiang@users.noreply.github.com>
2023-05-25 16:38:40 +0800

    Delete src directory

--- src/main/java/com/exist/project_plan/repository/ProjectRepository.java
+++
@@ -1,9 +0,0 @@
-package com.exist.project_plan.repository;
-
-import com.exist.project_plan.model.entity.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProjectRepository extends JpaRepository<Project,Long> {
-}
--- src/main/java/com/exist/project_plan/service/TaskService.java
+++
@@ -1,4 +0,0 @@
-package com.exist.project_plan.service;
-
-public interface TaskService {
-}
